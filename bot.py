e23c7555e6d5e03919d791bb4f564b4ac0d46821
YTFarman9758 <162802755+YTFarman9758@users.noreply.github.com>
2024-12-30 17:00:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ files = glob.glob(ppath)
 JisshuBot.start()
 loop = asyncio.get_event_loop()
 
-pyrogram.utils.MIN_CHANNEL_ID = -1002255407902
+pyrogram.utils.MIN_CHANNEL_ID = -1002332166273
 
 async def Jisshu_start():
     print('\n')
